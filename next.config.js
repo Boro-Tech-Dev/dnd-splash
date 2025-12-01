ff9dc3273f01f82ca0111db9a7c13015d7d63a92
Boro-Tech-Dev <matt@boro-tech.com>
2025-12-01 18:54:42 -0500

    Config update

--- next.config.js
+++ next.config.js
@@ -1,7 +1,19 @@
+const path = require('path')
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   poweredByHeader: false,
+  webpack: (config) => {
+    // Explicitly configure webpack aliases to match tsconfig.json paths
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.resolve(__dirname, '.'),
+      '@components': path.resolve(__dirname, 'components'),
+      '@lib': path.resolve(__dirname, 'lib'),
+    }
+    return config
+  },
 }
 
 module.exports = nextConfig
