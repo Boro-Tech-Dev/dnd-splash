fb6c8d2a3743d841cb454033aed042ef155ed570
ProbleMattic1 <matthew.kulcsar@gmail.com>
2026-05-26 11:52:06 -0400

    favicon fix

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,8 @@
+import { createRoot } from "react-dom/client";
+import App from "./app/App.tsx";
+import "./styles/index.css";
 
-  import { createRoot } from "react-dom/client";
-  import App from "./app/App.tsx";
-  import "./styles/index.css";
+const SITE_TITLE = "DeployDeliver";
+document.title = SITE_TITLE;
 
-  createRoot(document.getElementById("root")!).render(<App />);
-  
+createRoot(document.getElementById("root")!).render(<App />);
